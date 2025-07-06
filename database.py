3c53170b16d14a752d12f4005784226899acd5a6
IllyaKol777 <133062066+IllyaKol777@users.noreply.github.com>
2025-07-06 23:40:41 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://dava_bayer:kIa41WLPqldniK2zKCwTbKP7sG4ud3KP@dpg-d1l431er433s73d86e70-a/dava_bayer"
+DATABASE_URL = "postgresql://dava_bayer:oRHlidY8IDrDAfx0MTaYgFqfa4QCCZdM@dpg-d1ldt5h5pdvs73bsf0pg-a/dava_bayer_87zd"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
